4c19601f6426a289376357e86f9f9ec0d638e1f8
Michael <444647888@qq.com>
2019-02-27 22:07:56 +0800

    "03-揭标逻辑实现"

--- contract.sol
+++ contract.sol
@@ -21,7 +21,7 @@ contract Auction {
 
         uint highestBid;
         uint secondHighestBid;
-        address highestBidder;
+        address payable highestBidder;
         uint totalBids;
         //everyone could bid many times. bytes32 is the hash of the ideal price and the password
         mapping(address=>mapping(bytes32=>Bid)) bids;
@@ -64,13 +64,13 @@ contract Auction {
         productIdToOwmer[productIndex]=msg.sender;
     }
     //test function
-    function getProductById(uint _index)public view returns(uint,string memory,string memory,string memory,
-    string memory,uint,uint,uint,ProductStatus,ProductCondition){
-        address owner = productIdToOwmer[_index];
-        Product memory pro = stores[owner][_index];
-        return (pro.id,pro.name,pro.category,pro.imageLink,pro.descLink,pro.startPrice,pro.auctionStartTime,
-        pro.auctionEndTime,pro.status,pro.condition);
-    }
+    // function getProductById(uint _index)public view returns(uint,string memory,string memory,string memory,
+    // string memory,uint,uint,uint,ProductStatus,ProductCondition){
+    //     address owner = productIdToOwmer[_index];
+    //     Product memory pro = stores[owner][_index];
+    //     return (pro.id,pro.name,pro.category,pro.imageLink,pro.descLink,pro.startPrice,pro.auctionStartTime,
+    //     pro.auctionEndTime,pro.status,pro.condition);
+    // }
 
     struct Bid {
         uint productId;
@@ -97,19 +97,78 @@ contract Auction {
     }
 
     //test function
-    function getBidById(uint _productIndex,uint _idealPrice,string memory _password)
-    public view returns(uint,uint,bool,address){
-        address owner = productIdToOwmer[_productIndex];
-        bytes memory bidInfo = abi.encodePacked(_idealPrice,_password);
-        bytes32 bidBytes = keccak256(bidInfo);
-        Product storage pro = stores[owner][_productIndex];
-        Bid memory b = pro.bids[msg.sender][bidBytes];
-        return (b.productId,b.price2Show,b.isRevealed,b.bidder);
-    }
+    // function getBidById(uint _productIndex,uint _idealPrice,string memory _password)
+    // public view returns(uint,uint,bool,address){
+    //     address owner = productIdToOwmer[_productIndex];
+    //     bytes memory bidInfo = abi.encodePacked(_idealPrice,_password);
+    //     bytes32 bidBytes = keccak256(bidInfo);
+    //     Product storage pro = stores[owner][_productIndex];
+    //     Bid memory b = pro.bids[msg.sender][bidBytes];
+    //     return (b.productId,b.price2Show,b.isRevealed,b.bidder);
+    // }
 
     function getBalance() public view returns(uint){
         return address(this).balance;
     }
 
+
+    event revealEvent(uint _productIndex,bytes32 bidBytes,uint highestBid,uint sencondHighestBid,uint refund);
+
+    function revealBid(uint _productIndex,uint _idealPrice,string memory password)public {
+        address owner = productIdToOwmer[_productIndex];
+        Product storage pro = stores[owner][_productIndex];
+
+        bytes memory bidInfo = abi.encodePacked(_idealPrice,password);
+        bytes32 bidBytes = keccak256(bidInfo);
+
+        Bid storage curBid =pro.bids[msg.sender][bidBytes];
+        //check
+        require(!curBid.isRevealed);
+        //this bid is found,show that ur password is right
+        require(curBid.bidder!=address(0));
+        curBid.isRevealed=true;
+
+        uint confusedPrice = curBid.price2Show;
+        uint idealPrice = _idealPrice;
+        uint refund = 0;
+
+        if(confusedPrice<idealPrice){
+            refund = confusedPrice;
+        }else{
+            if(idealPrice>pro.highestBid){
+                //the first one to reveal bid
+                if(pro.highestBidder==address(0)){
+                    //update the product
+                    pro.highestBid=idealPrice;
+                    pro.secondHighestBid=pro.startPrice;
+                    pro.highestBidder=msg.sender;
+                    refund = confusedPrice - idealPrice;
+                }else{
+                    pro.highestBidder.transfer(pro.highestBid);
+                    pro.secondHighestBid = pro.highestBid;
+                    pro.highestBid = idealPrice;
+                    pro.highestBidder=msg.sender;
+                    refund = confusedPrice-idealPrice;
+                }
+            }else{
+                //
+                if(idealPrice>pro.secondHighestBid){
+                    pro.secondHighestBid = idealPrice;
+                }
+                refund = confusedPrice;
+            }
+        }
+
+
+        if(refund>0){
+            msg.sender.transfer(refund);
+        }
+        emit revealEvent(_productIndex,bidBytes,pro.highestBid,pro.secondHighestBid,refund);
+    }
+
+    function getInfoBack(uint _productIndex) public view returns(address,uint,uint,uint){
+        Product storage pro = stores[productIdToOwmer[_productIndex]][_productIndex];
+        return (pro.highestBidder,pro.highestBid,pro.secondHighestBid,pro.totalBids);
+    }
 
 }
